ef5ab31604a26ef32c64b2e0b8490d355c1daec6
David Anthoff <anthoff@berkeley.edu>
2015-06-26 16:43:51 -0700

    Fix small bug related to floating point conversion

--- src/cheb.jl
+++ src/cheb.jl
@@ -5,7 +5,7 @@
 function Basis(::Cheb, n::Int, a::Real, b::Real)  # chebdef
     n <= 0 && error("n must be positive")
     a >= b && error("left endpoint (a) must be less than right end point (b)")
-    a, b = map(Float64, (a, b))
+    a, b = map(float64, (a, b))
     Basis(Cheb(), n, a, b, ChebParams(n, a, b))
 end
 
