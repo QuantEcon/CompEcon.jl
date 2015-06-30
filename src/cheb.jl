50ec099a9b49c618bb2a2837f0c2b31e4d2c7937
David Anthoff <anthoff@berkeley.edu>
2015-06-29 17:03:02 -0700

    Remove unnecessary conversion

--- src/cheb.jl
+++ src/cheb.jl
@@ -5,7 +5,6 @@
 function Basis(::Cheb, n::Int, a::Real, b::Real)  # chebdef
     n <= 0 && error("n must be positive")
     a >= b && error("left endpoint (a) must be less than right end point (b)")
-    a, b = map(float64, (a, b))
     Basis(Cheb(), n, a, b, ChebParams(n, a, b))
 end
 
