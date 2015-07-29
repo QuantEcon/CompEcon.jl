b203b42cac2f736969dca80ae911377f1bd3dccf
Spencer Lyon <spencerlyon2@gmail.com>
2015-07-29 11:14:53 -0400

    TEST: finished testing basis.jl. Should have 100% coverage

--- test/basis.jl
+++ test/basis.jl
@@ -19,55 +19,65 @@ facts("Test Basis") do
     # directly construct multivariate basis using (default) inner constructor
     b_both = Basis{2}(bt, n, a, b, params)
 
+    # should be equivalent to Basis(b1, b1)
+    b_spline2d = Basis(Spline(), [n[1], n[1]], [a[1], a[1]], [b[1], b[1]], [1, 1])
+
+    # should be equivalent to Basis(b2, b2)
+    b_cheb2d = Basis(Cheb(), [n[2], n[2]], [a[2], a[2]], [b[2], b[2]])
+
     context("constructors") do
-        @fact map(x->isa(x, Basis{1}), (b1, b2)) => (true, true)
-        @fact isa(b_both, Basis{2}) => true
+        @fact map(x->isa(x, Basis{1}), (b1, b2)) --> (true, true)
+        @fact isa(b_both, Basis{2}) --> true
 
         # use convenience outer constructor to not supply type parameter when
         # constructing multi-varaite basis
-        @fact Basis(bt, n, a, b, params) => b_both
+        @fact Basis(bt, n, a, b, params) --> b_both
 
         # use Basis(Type, args...) constructor
-        @fact Basis(Spline, CompEcon.old_params(params[1])...) => b1
-        @fact Basis(Cheb, CompEcon.old_params(params[2])...) => b2
+        @fact Basis(Spline, CompEcon.old_params(params[1])...) --> b1
+        @fact Basis(Cheb, CompEcon.old_params(params[2])...) --> b2
 
         # use Basis(params) constructor
-        @fact Basis(params[1]) => b1
-        @fact Basis(params[2]) => b2
+        @fact Basis(params[1]) --> b1
+        @fact Basis(params[2]) --> b2
 
         # test Basis(b1, b2) format
-        @fact Basis(b1, b2) => b_both
+        @fact Basis(b1, b2) --> b_both
 
         # test unicode ×
-        @fact b1 × b2 => b_both
+        @fact b1 × b2 --> b_both
 
         # test Basis(params1, params2)
-        @fact Basis(params...) => b_both
+        @fact Basis(params...) --> b_both
+
+        # test fundefn type methods
+        @fact b_spline2d --> Basis(b1, b1)
+        @fact b_cheb2d --> Basis(b2, b2)
     end
 
     context("getindex and combining preserves basis") do
-        @fact b_both[1] => b1
-        @fact b_both[2] => b2
-        @fact Basis(b_both[1], b_both[2]) => b_both
+        @fact b_both[1] --> b1
+        @fact b_both[2] --> b2
+        @fact Basis(b_both[1], b_both[2]) --> b_both
         for b in [b1, b2]
-            @fact b[1] => b
+            @fact b[1] --> b
             @fact_throws ErrorException b[2]
         end
     end
 
     context("ndims, length, and size") do
         for b in [b1, b2]
-            @fact ndims(b) => 1
-            @fact length(b) => b.n[1]
-            @fact size(b) => (b.n[1],)
-            @fact size(b, 1) => b.n[1]
+            @fact ndims(b) --> 1
+            @fact length(b) --> b.n[1]
+            @fact size(b) --> (b.n[1],)
+            @fact size(b, 1) --> b.n[1]
             @fact_throws ErrorException size(b, 2)
         end
 
-        @fact ndims(b_both) => 2
-        @fact length(b_both) => prod(n)
-        @fact size(b_both) => tuple(n...)
-        @fact size(b_both, 1) => n[1]
+        @fact ndims(b_both) --> 2
+        @fact length(b_both) --> prod(n)
+        @fact size(b_both) --> tuple(n...)
+        @fact size(b_both, 1) --> n[1]
 
     end
 
@@ -82,17 +92,17 @@ facts("Test Basis") do
 
         # test the nodes from basis 1 are what we expect, are the same as the
         # corresponding nodes from the 2d basis and have the correct length
-        @fact n1 => collect(linspace(-3, 3, 9))
-        @fact n1 => n1_both
-        @fact length(n1) => b1.n[1]
+        @fact n1 --> collect(linspace(-3, 3, 9))
+        @fact n1 --> n1_both
+        @fact length(n1) --> b1.n[1]
 
         # test that the nodes from basis 2 are the same as corresponding nodes
         # from 2d basis and have correct length
-        @fact n2 => n2_both
-        @fact length(n2) => b2.n[1]
+        @fact n2 --> n2_both
+        @fact length(n2) --> b2.n[1]
 
         # verify that the nodes from combined 2d basis is correct size
-        @fact size(n_both) => (length(n1)*length(n2), 2)
+        @fact size(n_both) --> (length(n1)*length(n2), 2)
     end
 end
 
