32b8d599ea41765c84c230b136bdcc0394202700
Spencer Lyon <spencerlyon2@gmail.com>
2015-08-05 16:25:23 -0400

    ENH: proper univariate version of golden

--- src/optimization.jl
+++ src/optimization.jl
@@ -1,6 +1,5 @@
 function golden_method(f::Function, a::AbstractVector, b::AbstractVector;
                        tol=eps()*10, maxit=1000)
-
     α1 = (3 - sqrt(5)) / 2
     α2 = 1 - α1
     d = b - a
@@ -33,7 +32,56 @@ function golden_method(f::Function, a::AbstractVector, b::AbstractVector;
     x1, f1
 end
 
-function golden_method(f::Function, a::Real, b::Real; tol=eps()*10, maxit=1000)
-    x1, f1 = golden_method(f, [a], [b]; tol=tol, maxit=maxit)
-    x1[1], f1[1]
+# function golden_method(f::Function, a::AbstractVector, b::AbstractVector;
+#                        tol=eps()*10, maxit=1000)
+#     out_x = similar(a)
+#     out_f = similar(a)
+#     N = length(a)
+#     @inbounds for i=1:N
+#         out_x[i], out_f[i] = golden_method(f, a[i], b[i]; tol=tol, maxit=maxit)
+#     end
+#     out_x, out_f
+# end
+
+function golden_method{T<:Real}(f::Function, a::T, b::T; tol::Float64=10*eps(),
+                                maxit::Int=1000)
+    α1 = (3 - sqrt(5)) / 2
+    α2 = 1 - α1
+    d = b - a
+    x1 = a + α1*d
+    x2 = a + α2*d
+    s = one(T)
+    f1 = f(x1)
+    f2 = f(x2)
+
+    d = α1*α2*d
+
+    it = 0
+
+    while d .> tol && it < maxit
+        it += 1
+
+        d *= α2
+
+        if f2 > f1
+            x1 = x2
+            f1 = f2
+            x2 = x1 + s*d
+        else
+            x2 = x1 - s*d
+        end
+
+        s = sign(x2 - x1)
+
+        f2 = f(x2)
+    end
+
+    it >= maxit && warn("`golden_method`: maximum iterations exceeded")
+
+    if f2 > f1
+        x1 = x2
+        f1 = f2
+    end
+
+    x1, f1
 end
