d0c43b2eb52318c4aeef6cd1976a59e6a5aa7322
albep <alberto.polo@nyu.edu>
2015-08-13 15:49:56 +0200

    ENH: Added new test files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 test_files = ["types.jl", "basis.jl", "optimization.jl", "util.jl",
-              "quad.jl",
+              "quad.jl", "spline.jl",	"interp.jl",
               "basis_structure.jl"]
 
 for f in test_files
