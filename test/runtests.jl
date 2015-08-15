ea4090bc15f5662d76e5ce7d57b33f54308f4b97
albep <alberto.polo@nyu.edu>
2015-08-15 15:19:15 +0200

    ENH: Added test cheb.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 test_files = ["types.jl", "basis.jl", "optimization.jl", "util.jl",
-              "quad.jl", "spline.jl",	"interp.jl",
+              "quad.jl", "spline.jl",	"interp.jl", "cheb.jl",
               "basis_structure.jl"]
 
 for f in test_files
