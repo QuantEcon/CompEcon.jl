4c748b5245bf2817c0f724c04d15bc21578d7f47
Spencer Lyon <spencerlyon2@gmail.com>
2016-04-15 14:20:25 -0400

    Also rely on QE for quad

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 test_files = ["types.jl", "basis.jl", "optimization.jl", "util.jl",
-              "quad.jl", "spline.jl",	"interp.jl", "cheb.jl", "lin.jl",
+              "spline.jl",	"interp.jl", "cheb.jl", "lin.jl",
               "basis_structure.jl", "zeros.jl", "complete.jl"]
 
 for f in test_files
