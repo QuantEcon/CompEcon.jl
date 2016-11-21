4343dac5ea1b0e24e30309deb55cc892e6b50421
EikiTakigawa <E.Shine@outlook.jp>
2016-11-21 17:42:33 +0900

    Change "LinParam" to "LinParams"

--- src/lin.jl
+++ src/lin.jl
@@ -9,17 +9,17 @@ function lindef(breaks::Vector, evennum::Int=0)
 end
 
 # linnode.m -- DONE
-linnode(breaks, evennum) = nodes(LinParam(breaks, evennum))
+linnode(breaks, evennum) = nodes(LinParams(breaks, evennum))
 
 # lindop.m
 function lindop(breaks, evennum, order)
-    D, params = derivative_op(LinParam(breaks, evennum), order)
+    D, params = derivative_op(LinParams(breaks, evennum), order)
     n, a, b = length(params.breaks), params.breaks[1], params.breaks[end]
     D, n, a, b, params
 end
 
 # linbase.m -- DONE
 function linbase(breaks, evennum=0, x=breaks, order=0)
-    B = evalbase(LinParam(breaks, evennum), x, order)
+    B = evalbase(LinParams(breaks, evennum), x, order)
     B, x
 end
